f4c4355341eedf91b60ea550bf248e4e9472b763
David Mora <David.mora@codeimmersives.com>
2019-11-10 22:16:51 -0500

    created new schema to house the queue

---
+++ vans_backend/src/models/Queue.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const moment = require('moment');
+
+let QueueSchema = new Schema({
+
+})
+
+module.exports = mongoose.model("Queue", QueueSchema)
