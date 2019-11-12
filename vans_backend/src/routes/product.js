acc0379635267fc227a13433675620536bc718f6
David Mora <David.mora@codeimmersives.com>
2019-11-12 08:14:25 -0500

    made comments in code

--- vans_backend/src/routes/product.js
+++ vans_backend/src/routes/product.js
@@ -5,9 +5,9 @@ const router = express.Router();
 let productController = require('../controllers/productController');
 
 router.get('/all-categories', productController.getAllCategories);
-
+//* the id is not the id of the shoe style but the category for which it is nested in
 router.get('/all-shoe-style-by-id/:categoryID', productController.getShoeStyleById);
-
+//* same goes for products. it uses the id of the shoestyle 
 router.get('/all-product-by-id/:shoeStyleID', productController.getProductById);
 
 module.exports = router;
