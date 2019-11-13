013b23764e6b0e84ad78ee42ff07bedbf15a7b65
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-11-13 14:41:40 -0500

    added category buttons for fetching

--- vans_expo_front/screens/Floor.js
+++ vans_expo_front/screens/Floor.js
@@ -28,9 +28,11 @@ class FloorScreen extends Component {
 
   componentDidMount() {}
 
-  handleOnSubmit = () => {};
+  handleGetCategory = () => {};
+  handleGetStyle = () => {};
+  handleGetProduct = () => {};
+  handleSendToStock = () => {};
 
-  someFunction = () => {};
 
   render() {
     
@@ -46,6 +48,21 @@ class FloorScreen extends Component {
               receivedBy: "5dc1b98f20ada9e222c66a5f"
             }
           }})}
+        />
+         <Button
+          title={"ADULTS"}
+          style={styles.input}
+          onPress={this.handleGetCategory}
+        />
+         <Button
+          title={"Kids"}
+          style={styles.input}
+          onPress={this.handleGetCategory}
+        />
+         <Button
+          title={"Toddlers"}
+          style={styles.input}
+          onPress={this.handleGetCategory}
         />
       </View>
     );
