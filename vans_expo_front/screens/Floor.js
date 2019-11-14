7529053a2af557e57acc64c141f64aba52954047
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-11-14 09:38:31 -0500

    added opacity handler to list items

--- vans_expo_front/screens/Floor.js
+++ vans_expo_front/screens/Floor.js
@@ -1,28 +1,47 @@
 // product  - category(adults, kids, toddlers) - shoes(styles i.e. "Old Schools") // needs modal for selecting
 
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Image, Button } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  TouchableOpacity
+} from "react-native";
 import { connect } from "react-redux";
 import { addToQueue } from "../redux/actions/dataPassingActions";
 import { getAllCategories } from "../redux/actions/floorActions";
 import { ListItem, Card } from "react-native-elements";
 class FloorScreen extends Component {
+  state = {
+    isModalVisible: false
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
   componentDidMount() {
     // let categories = this.props.getAllCategories();
     // console.log(categories);
-    this.props.navigation.setParams({
-      handleGetAllCategories: this.props.getAllCategories()
-    });
-    console.log(categories);
+    // this.props.navigation.setParams({
+    //   handleGetAllCategories: this.props.getAllCategories()
+    // });
+    // console.log(categories);
   }
   toggleModal = () => {
     this.setState({ modalVisisble: true });
   };
 
   render() {
+    const list = Object.keys(this.props.floorInfo);
     return (
-      <View>
-        <Text>{this.props.floorInfo}</Text>
+      <View style={styles.screen}>
+        {list.map((list, i) => (
+          <TouchableOpacity onPress={() => this.toggleModal}>
+            <ListItem key={i} title={list[i]} bottomDivider />
+          </TouchableOpacity>
+        ))}
       </View>
     );
   }
